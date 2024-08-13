797adcf8331e412347b23f9640a23981b3ef524f
anyoneout <chrisdafur@gmail.com>
2024-08-12 20:09:01 -0500

    finished day1

--- day-1.js
+++ day-1.js
@@ -14,10 +14,22 @@ console.log(book.title);  // Output: To Kill a Mockingbird
 console.log(book.author); // Output: Harper Lee
 console.log(book.pages);  // Output: 281
 
+
 // TODO 1: Create a `movie` object
 // Create an object named `movie` with the following properties:
 // `title` (string), `director` (string), `year` (number)
+const movie = {
+  title: "Big Trouble in Little China",
+  director: "John Carpenter",
+  year: 1986
+};
+
 // Log each property using dot notation.
+
+console.log("\nMovie Object");
+console.log(movie.title);
+console.log(movie.director);
+console.log(movie.year);
 
 
 // Example 2: Creating a Pet Object
@@ -35,8 +47,17 @@ console.log(pet.age);     // Output: 5
 // TODO 2: Create a `city` object
 // Create an object named `city` with the following properties:
 // `name` (string), `population` (number), `country` (string)
+const city = {
+  name: "New Orleans",
+  population: 369749,
+  country: "United States"
+}
 // Log each property using dot notation.
 
+console.log("\nCity Object");
+console.log(city.name);
+console.log(city.population);
+console.log(city.country);
 
 // Example 3: Creating a Car Object
 const car = {
@@ -53,8 +74,18 @@ console.log(car.year);   // Output: 2021
 // TODO 3: Create a `computer` object
 // Create an object named `computer` with the following properties:
 // `brand` (string), `processor` (string), `ram` (number)
+
+const computer = {
+  brand: "Dell",
+  processor: "Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz   3.19 GHz",
+  ram: 16
+}
 // Log each property using dot notation.
 
+console.log("\nComputer Object");
+console.log(computer.brand);
+console.log(computer.processor); 
+console.log(computer.ram);   
 
 // Example 4: Creating a Product Object
 const product = {
@@ -71,8 +102,18 @@ console.log(product.inStock); // Output: true
 // TODO 4: Create a `restaurant` object
 // Create an object named `restaurant` with the following properties:
 // `name` (string), `type` (string), `rating` (number)
+
+const restaurant = {
+  name: "Cane and Table",
+  type: "cuban",
+  rating: 4.5
+}
 // Log each property using dot notation.
 
+console.log("\nRestaurant Object");
+console.log(restaurant.name);
+console.log(restaurant.type); 
+console.log(restaurant.rating);   
 
 // Example 5: Creating a Student Object
 const student = {
@@ -89,5 +130,16 @@ console.log(student.grade);     // Output: A
 // TODO 5: Create a `house` object
 // Create an object named `house` with the following properties:
 // `address` (string), `size` (number), `hasGarage` (boolean)
+
+const house = {
+  address: "2530 Burgundy",
+  size: 1500,
+  hasGarage: true
+}
+
+console.log("\nHouse Object");
+console.log(house.address);
+console.log(house.size); 
+console.log(house.hasGarage);
 // Log each property using dot notation.
 
